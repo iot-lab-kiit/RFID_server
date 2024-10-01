0e0fbeda014537b0f7e3d50b3daeb999e0509815
Akangkha <akangkhasarkar@gmail.com>
2024-10-02 00:42:59 +0530

    project initialisation

---
+++ directus.js
@@ -0,0 +1,23 @@
+import {
+  createDirectus,
+  rest,
+  authentication,
+  staticToken,
+} from "@directus/sdk";
+import dotenv from "dotenv";
+dotenv.config();
+export const client = createDirectus(process.env.PUBLIC_DIRECTUS_URL)    //static token
+  .with(rest())
+  .with(
+    authentication({
+      mode: "json",
+      staticToken: process.env.TOKEN,
+    })
+  );
+
+export const clientToken = (token) => {     //dynamic token
+  return createDirectus(process.env.PUBLIC_DIRECTUS_URL) 
+    .with(staticToken(token))
+    .with(rest());
+};
+
