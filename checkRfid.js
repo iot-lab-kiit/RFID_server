bdc2a2e8d431e451b3366b0e7f9a6df06ab87267
Vidhi425 <hcpsvidhi25@gmail.com>
2024-10-02 17:04:00 +0530

    rfid tage check

---
+++ checkRfid.js
@@ -0,0 +1,32 @@
+import { readItems } from '@directus/sdk';
+import { client } from './directus.js'; 
+
+async function checkRFIDTag(rfidTag) {
+  try {
+  
+    const response = await client.request(readItems('teams', {
+      filter: {
+        rfid_tag: {
+          _eq: rfidTag,
+        },
+      },
+      limit: 1, 
+      fields: ['id', 'name', 'email', 'rfid_tag', 'user_created'], 
+    }));
+    console.log(response)
+
+    const team = response.data[0]; 
+
+    if (team) {
+      console.log('RFID Tag exists:', team);
+      console.log('User ID:', team.id);
+      console.log('Team Name:', team.name); 
+    } else {
+      console.log('RFID Tag does not exist.');
+    }
+  } catch (error) {
+    console.error('Error fetching RFID tag:', error);
+  }
+}
+
+checkRFIDTag(12546789);
